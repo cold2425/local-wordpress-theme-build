c17595b8d1053ca12fa4cf912aa43fc9949b99fa
cold2425 <pawel.warachowski@fibbo.pl>
2022-03-30 18:04:12 +0200

    Separated localhostUrl and themePath.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,29 @@
+// config
+const localhostUrl = "http://localhost/WordpressThemeBuildconfig/";
+const themePath = "wp-content/themes/wordpressthemebuildconfig";
+
+// reuqire
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
-const csso = require('gulp-csso');
-const connect = require('gulp-connect-php');
-const browserSync = require('browser-sync');
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const csso = require("gulp-csso");
+const connect = require("gulp-connect-php");
+const browserSync = require("browser-sync");
 
 const server = function(cb) {
-
     browserSync.init({
         notify: false,
-        proxy: 'http://localhost/WordpressThemeBuildconfig/',
+        proxy: localhostUrl,
         port: 3000,
         open: true
     });
-
     cb();
-
 }
 
 const css = function() {
-    return gulp.src("wp-content/themes/wordpressthemebuildconfig/includes/sass/main.scss")
+    return gulp.src(themePath + "/includes/sass/main.scss")
         .pipe(sourcemaps.init())
         .pipe(
             sass({
@@ -35,17 +37,15 @@ const css = function() {
             basename: "styles"
         }))
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("wp-content/themes/wordpressthemebuildconfig/includes/css"))
+        .pipe(gulp.dest(themePath + "/includes/css"))
         .pipe(browserSync.stream());
 }
 
 const watch = function(cb) {
-    gulp.watch("wp-content/themes/wordpressthemebuildconfig/includes/sass/*.scss", gulp.series(css));
+    gulp.watch(themePath + "/includes/sass/*.scss", gulp.series(css));
     gulp.watch('**/*.php').on('change', browserSync.reload);
     gulp.watch('**/*.js').on('change', browserSync.reload);
     cb();
 }
 
 exports.default = gulp.series(css, server, watch);
-exports.css = css;
-exports.watch = watch;
